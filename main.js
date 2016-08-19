073fc37e8daa1d8eb8f1f71431e4915780a38c02
Sam Goldsmith <samggold@gmail.com>
2016-08-19 15:11:15 -0600

    finished greaterThan10

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 //First Exercise
-//Function needs to evaluate an array and return whether it contains numbers great than 10
+//Function needs to evaluate an array and return whether it contains numbers greater than 10
 //any([8, 9, 10, 11], greaterThan10)
 //hint: greaterThan10 is a callback
-var array = [7, 8, 9, 10, 11];
+var array1 = [7, 8, 9, 10, 11];
 var array2 = [1, 2, 3, 4];
 function greaterThan10(array) {
-  for (var i=0; i < array.length; i++) {
-    if (array[i] >= 10) {
+  array.forEach(function(number) {
+    if (number > 10) {
       return true;
     } else {
       return false;
     }
-  }
+  });
 }
-console.log(greaterThan10(array));
-console.log(greaterThan10(array2));
+
+function any(array, callBackFn) {
+  console.log(array);
+  console.log(callBackFn);
+  return callBackFn(array);
+}
+console.log(any(array1, greaterThan10));
+console.log(any(array2, greaterThan10));
 
 //Second Exercise
 //Function that returns the string 'surpise!'
@@ -33,28 +39,29 @@ function surprise() {
 //hint: onlyOdd is a callback
 var arrayOfNums = [1, 2, 3, 4, 8, 3, 4, 6, 9, 0, 11, 23];
 function returnOdd(array) {
-  var newArr = [];
+  var newArrOfOdds = [];
   for (var i=0; i < array.length; i++) {
     if (array[i] % 2 > 0) {
-      newArr.push(array[i]);
+      newArrOfOdds.push(array[i]);
     }
   }
-  return newArr;
+  return newArrOfOdds;
 }
 function filter(array, callBackFn) {
   var onlyOddArray = [];
   return onlyOddArray = callBackFn(array);
 }
-// console.log(filter(arrayOfNums, returnOdd));
+
+//console.log(filter(arrayOfNums, returnOdd));
 
 
 //Fourth Exercise
 //Add together two numbers
 //sumTwoNumbers(3)(9)
 //hint: return a Function
-function sumTwoNumbers(thingy) {
+function sumTwoNumbers(num1) {
   return function(num2) {
-    return thingy + num2;
+    return num1 + num2;
   };
 }
 // console.log(sumTwoNumbers(2)(4));
